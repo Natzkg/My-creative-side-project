6395d06eec48ad127dd9940902b2920fe094162a
Natzkg <108194075+Natzkg@users.noreply.github.com>
2022-09-28 15:51:34 -0400

    First coding project
    My very start at coding journey and first project from SheCodes basics worksop.

---
+++ script.js
@@ -0,0 +1,10 @@
+function share() {
+    let name = prompt("Hi, what's your name?");
+    let project = prompt("What is your latest project?");
+    
+   
+     alert ("Great job!🔥 Keep up the good work!👍");
+    }
+   
+    let shareButton = document.querySelector("button");
+    shareButton.addEventListener("click", share);
